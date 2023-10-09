a2ea1fb7592ed99d5cf891e7095bdd4b13bf4505
Rijwa Ali <146122433+RijwaAli@users.noreply.github.com>
2023-10-09 21:09:33 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,76 +1,173 @@
-﻿namespace My__Ride
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment_3
 {
-    internal class Program
+    /// <summary>
+    /// task 2
+    /// </summary>
+    class Program
     {
-        private admin admn;
-        private List<Ride> rides;
-
-        public Program()
+        static void Main()
         {
-            admn = new admin();
-            rides = new List<Ride>();
-        }
+            Dictionary<int, string> studentDB = new Dictionary<int, string>();
+            Console.WriteLine("\n");
+            Console.WriteLine("\t\t\t\t\t----------------------------------------------\t\t\t\t\t");
+            Console.WriteLine("\t\t\t\t\t\t\tSTUDENT DATABASE \t\t\t\t\t\t");
+            Console.WriteLine("\t\t\t\t\t----------------------------------------------\t\t\t\t\t");
+            Console.WriteLine("\n");
 
-        public void ShowMainMenu()
-        {
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("WELCOME TO MY__RIDE");
-                Console.WriteLine("-------------------");
-                Console.WriteLine("1. Book a Ride");
-                Console.WriteLine("2. Enter as Driver");
-                Console.WriteLine("3. Enter as Admin");
-                Console.WriteLine("4. Exit");
-                Console.Write("Press 1 to 4 to select an option: ");
-
-                int choice;
-                if (int.TryParse(Console.ReadLine(), out choice))
+                try
                 {
-                    switch (choice)
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Student Database Menu:");
+                    Console.WriteLine("1. Add Student");
+                    Console.WriteLine("2. View Students");
+                    Console.WriteLine("3. Search Student by ID");
+                    Console.WriteLine("4. Update Student Name");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("Enter your choice: ");
+
+                    if (int.TryParse(Console.ReadLine(), out int choice))
                     {
-                        case 1:
-                            BookRide();
-                            break;
-                        case 2:
-                            EnterAsDriver();
-                            break;
-                        case 3:
-                            EnterAsAdmin();
-                            break;
-                        case 4:
-                            Environment.Exit(0);
-                            break;
-                        default:
-                            Console.WriteLine("Invalid choice. Please select a valid option.");
-                            break;
+                        switch (choice)
+                        {
+                            case 1:
+                                AddStudent(studentDB);
+                                break;
+                            case 2:
+                                ViewStudents(studentDB);
+                                break;
+                            case 3:
+                                SearchStudent(studentDB);
+                                break;
+                            case 4:
+                                UpdateStudent(studentDB);
+                                break;
+                            case 5:
+                                Console.WriteLine("Exit");
+                                return;
+                            default:
+                                Console.WriteLine("Invalid choice. Please enter a valid option.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid choice.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+
+        static void AddStudent(Dictionary<int, string> database)
+        {
+            try
+            {
+                Console.Write("Enter Student ID: ");
+                if (int.TryParse(Console.ReadLine(), out int studentID))
+                {
+                    Console.Write("Enter Student Name: ");
+                    string studentName = Console.ReadLine();
+
+                    if (!database.ContainsKey(studentID))
+                    {
+                        database.Add(studentID, studentName);
+                        Console.WriteLine("Student added successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Student with the same ID already exists.");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input. Please enter a valid menu option.");
+                    Console.WriteLine("Invalid input. Please enter a valid Student ID.");
                 }
-
-                Console.WriteLine("Press Enter to continue...");
-                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-        private void BookRide()
+
+        static void ViewStudents(Dictionary<int, string> database)
         {
+            try
+            {
+                Console.WriteLine("Student Database:");
+                foreach (var kvp in database)
+                {
+                    Console.WriteLine($"Student ID: {kvp.Key}, Name: {kvp.Value}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
 
-        private void EnterAsDriver()
+        static void SearchStudent(Dictionary<int, string> database)
         {
+            try
+            {
+                Console.Write("Enter Student ID to search: ");
+                if (int.TryParse(Console.ReadLine(), out int studentID))
+                {
+                    if (database.ContainsKey(studentID))
+                    {
+                        Console.WriteLine($"Student ID: {studentID}, Name: {database[studentID]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No information regarding this id found in the database.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid Student ID.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
 
-        private void EnterAsAdmin()
+        static void UpdateStudent(Dictionary<int, string> database)
         {
-        }
-
-        public static void Main(string[] args)
-        {
-            Program menu = new Program();
-            menu.ShowMainMenu();
+            try
+            {
+                Console.Write("Enter Student ID to update: ");
+                if (int.TryParse(Console.ReadLine(), out int studentID))
+                {
+                    if (database.ContainsKey(studentID))
+                    {
+                        Console.Write("Enter new name: ");
+                        string newName = Console.ReadLine();
+                        database[studentID] = newName;
+                        Console.WriteLine("Updated successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No information regarding this id found in the database.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid Student ID.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
